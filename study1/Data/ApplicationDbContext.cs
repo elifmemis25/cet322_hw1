e3b5d6953cfebcf38b433295919989ebd5f401e6
elifmemis25 <75388987+elifmemis25@users.noreply.github.com>
2021-05-05 19:09:56 -0400

    Proje dosyası ekle.

---
+++ study1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using study1.Models;
+
+namespace study1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    public DbSet<Student> Students { get; set; }
+
+    }
+}
---
+++ study1/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace study1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+
+    }
+}
